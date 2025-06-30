378b103924902de700b4b69c2671803a89922f1f
woohyuckk <woohyuckk@gmail.com>
2025-06-30 15:20:34 +0900

    del: 필요없는 테스트 컴포넌트 제거

--- src/components/memo-box.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const MemoBox = () => {
-  return <div className="border-2 p-4 font-bold">MemoBox</div>;
-};
--- src/stories/memo-box.tsx
+++
@@ -1,3 +0,0 @@
-export const MemoBox = () => {
-  return <div className="border-2 border-cyan-600 p-4 font-bold text-white">MemoBox 이지은 바잉</div>;
-};
